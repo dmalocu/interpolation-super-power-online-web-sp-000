418623c6aa33171a60264abef11908bc1fda36e7
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-05 03:47:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,4 @@ color = ["R: red, ", "O: orange, ", "Y: yellow, ", "G: green, ", "B: blue, ", "I
 def display_rainbow(color)
   puts "#{color[0]}" + "#{color[1]}" + "#{color[2]}" + "#{color[3]}" + "#{color[4]}" + "#{color[5]}" + "#{color[6]"
 end
+display_rainbow(color)
